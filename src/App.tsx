eb1c05dd321b85f4a4d4ed333bcf12ac9429e2cd
Ito <seongjin2427@gmail.com>
2022-05-22 03:13:38 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      This is Portfolio of
+      <br /> S E O N G J I N
+    </div>
+  );
+}
+
+export default App;
